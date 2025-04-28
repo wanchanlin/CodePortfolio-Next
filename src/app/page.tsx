9913fb3686fba997c8f920e1737d43f65b6657b2
Joyce Lin <ohanalin@gmail.com>
2025-04-28 15:44:31 -0400

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,57 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import ProjectCard from '../components/ProjectCard'
-import ContactForm from '../components/ContactForm'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import ProjectCard from "../components/ProjectCard";
+import ContactForm from "../components/ContactForm";
 
 const projects = [
   {
-    number: '01',
-    title: 'Balance of Power',
-    description: 'A project admin can manage national park info and the users can view and save their',
-    technologies: ['Html', 'Css', 'Js'],
-    imageSrc: '/images/project3.svg',
-    link: '/projects/nationalParks'
+    number: "01",
+    title: "Balance of Power",
+    description:
+      "A project admin can manage national park info and the users can view and save their",
+    technologies: ["Html", "Css", "Js"],
+    imageSrc: "/images/project3.svg",
+    link: "/projects/nationalParks",
   },
   {
-    number: '02',
-    title: 'Bird IP',
-    description: 'A project admin can manage national park info and the users can view and save their',
-    technologies: ['Html', 'Css', 'Js', 'php'],
-    imageSrc: '/images/project3.svg',
-    link: '/projects/nationalParks'
+    number: "02",
+    title: "Bird IP",
+    description:
+      "A project admin can manage national park info and the users can view and save their",
+    technologies: ["Html", "Css", "Js", "php"],
+    imageSrc: "/images/project3.svg",
+    link: "/projects/nationalParks",
   },
   {
-    number: '03',
-    title: 'National Parks Ecology',
-    description: 'A project admin can manage national park info and the users can view and save their',
-    technologies: ['Html', 'Css', 'Js', 'php', 'mysql'],
-    imageSrc: '/images/project3.svg',
-    link: '/projects/nationalParks'
+    number: "03",
+    title: "National Parks Ecology",
+    description:
+      "A project admin can manage national park info and the users can view and save their",
+    technologies: ["Html", "Css", "Js", "php", "mysql"],
+    imageSrc: "/images/project3.svg",
+    link: "/projects/nationalParks",
   },
   {
-    number: '04',
-    title: 'Earth V.S. Mars',
-    description: 'A project that explores the differences between Earth and Mars by using APIs',
-    technologies: ['pug', 'Css', 'node', 'Js'],
-    imageSrc: '/images/project2.svg',
-    link: '/projects/weather'
+    number: "04",
+    title: "Earth V.S. Mars",
+    description:
+      "A project that explores the differences between Earth and Mars by using APIs",
+    technologies: ["pug", "Css", "node", "Js"],
+    imageSrc: "/images/project2.svg",
+    link: "/projects/weather",
   },
   {
-    number: '05',
-    title: 'ANIMATION PROJECT',
-    description: 'joyce\'s first coding project',
-    technologies: ['Html', 'Css', 'Js'],
-    imageSrc: '/images/project1.svg',
-    link: '/projects/birdanimation'
-  }
-]
+    number: "05",
+    title: "ANIMATION PROJECT",
+    description: "joyce's first coding project",
+    technologies: ["Html", "Css", "Js"],
+    imageSrc: "/images/project1.svg",
+    link: "/projects/birdanimation",
+  },
+];
 
 function openService(serviceName: string) {
   // Implementation will be added later
@@ -61,21 +65,26 @@ export default function Home() {
           <div className="grid-container">
             <div className="grid-item">
               <div className="project-content text-white">
-                <h3>Hi, I am joyce</h3>
-                <h1 className="m-0">I am a full-stack developer + designer. <span className="typing-cursor">|</span></h1>
-                <h3>Base in TW CA</h3>
+                <span className="subtitle">Hi, I am joyce</span>
+                <br />
+                <span className="title">
+                  I am a full-stack developer + designer.{" "}
+                  <span className="typing-cursor">|</span>
+                </span>
+                <br />
+                <span>Base in TW CA</span>
+                <br />
                 <Link href="https://drive.google.com/file/d/1m5Zy2mz5g-fkOUsSy4ZigErFfSVLciCl/view?usp=sharing">
                   <div className="btn">Resume</div>
                 </Link>
               </div>
             </div>
             <div className="grid-item">
-              <iframe 
+              <iframe
                 className="responsive-iframe"
-                src='https://my.spline.design/developwe-f3cc7a88c680ba4cf806a9340e3e9ede/'
-                frameBorder='0'
-                width='100%'
-                height='100%'
+                src="https://my.spline.design/developwe-f3cc7a88c680ba4cf806a9340e3e9ede/"
+                width="100%"
+                height="100%"
               />
             </div>
           </div>
@@ -84,13 +93,15 @@ export default function Home() {
         <section>
           <h2 id="about">{`{ PROFILE }`}</h2>
           <p>
-            As a full-stack designer/developer with over 9 years of experience in the technology industry, I
-            specialize in creating visually stunning designs for various media, including graphic design, video
-            production, and motion graphics.
-            I have a strong foundation in the Adobe Creative Suite and have worked with clients in the
-            entertainment, fashion, and tech industries.
-            I am constantly seeking to improve my skills and stay up-to-date on the latest design trends.
-            If you have a project that could benefit from my expertise, please don't hesitate to reach out.
+            As a full-stack designer/developer with over 9 years of experience
+            in the technology industry, I specialize in creating visually
+            stunning designs for various media, including graphic design, video
+            production, and motion graphics. I have a strong foundation in the
+            Adobe Creative Suite and have worked with clients in the
+            entertainment, fashion, and tech industries. I am constantly seeking
+            to improve my skills and stay up-to-date on the latest design
+            trends. If you have a project that could benefit from my expertise,
+            please don't hesitate to reach out.
             <span className="typing-cursor">|</span>
           </p>
         </section>
@@ -98,28 +109,81 @@ export default function Home() {
         <section>
           <h2 id="services">{`{ SERVICES }`}</h2>
           <div className="tabs">
-            <button className="tablinks" onClick={() => openService('Program')}>Program</button>
-            <button className="tablinks" onClick={() => openService('Design')}>Design</button>
+            <button className="tablinks" onClick={() => openService("Program")}>
+              Program
+            </button>
+            <button className="tablinks" onClick={() => openService("Design")}>
+              Design
+            </button>
           </div>
 
           <div id="Program" className="tabcontent">
             <div className="card">
               <div className="service-content">
-                <Image className="icon" src="/images/code.png" alt="" width={50} height={50} />
+                <Image
+                  className="icon"
+                  src="/images/code.png"
+                  alt=""
+                  width={50}
+                  height={50}
+                />
                 <h3>Program</h3>
                 <p>
-                  Creating dynamic and responsive websites using the latest web technologies. I ensure that
-                  your website is not only visually appealing but also functional and user-friendly.
+                  Creating dynamic and responsive websites using the latest web
+                  technologies. I ensure that your website is not only visually
+                  appealing but also functional and user-friendly.
                   <span className="typing-cursor">|</span>
                 </p>
                 <div className="iconcontainer">
-                  <Image className="icon" src="/images/Html.svg" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/Css.svg" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/Js.svg" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/C.png" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/git.png" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/node.svg" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/react.png" alt="" width={30} height={30} />
+                  <img
+                    className="icon"
+                    src="/images/Html.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/Css.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/Js.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/C.png"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/git.png"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/node.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/react.png"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
                 </div>
               </div>
             </div>
@@ -128,19 +192,51 @@ export default function Home() {
           <div id="Design" className="tabcontent">
             <div className="card">
               <div className="service-content">
-                <Image className="icon" src="/images/design.png" alt="" width={50} height={50} />
+                <Image
+                  className="icon"
+                  src="/images/design.png"
+                  alt=""
+                  width={50}
+                  height={50}
+                />
                 <h3>Design</h3>
                 <p>
-                  Crafting visually stunning designs for various media, including graphic design, video
-                  production, and motion graphics. I have a strong foundation in the Adobe Creative Suite
-                  and have worked with clients in the entertainment, fashion, and tech industries.
+                  Crafting visually stunning designs for various media,
+                  including graphic design, video production, and motion
+                  graphics. I have a strong foundation in the Adobe Creative
+                  Suite and have worked with clients in the entertainment,
+                  fashion, and tech industries.
                   <span className="typing-cursor">|</span>
                 </p>
                 <div className="iconcontainer">
-                  <Image className="icon" src="/images/Figma.svg" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/photoshop.png" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/illustrator.png" alt="" width={30} height={30} />
-                  <Image className="icon" src="/images/aftereffects.svg" alt="" width={30} height={30} />
+                  <Image
+                    className="icon"
+                    src="/images/Figma.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/photoshop.png"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/illustrator.png"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                  <Image
+                    className="icon"
+                    src="/images/aftereffects.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
                 </div>
               </div>
             </div>
@@ -158,18 +254,47 @@ export default function Home() {
         <div className="crop">
           <div className="logo-container">
             <div className="logo-marquee">
-              <Link href="https://wanchanlin.webflow.io/work/rakuten" target="_blank" rel="noopener noreferrer">
-                <Image className="companylogo" src="/images/rakuten.svg" alt="Rakuten"alt="XingMobility" width={0} height={0} style={{width: 'auto', height: '50px'}}  />
+              <Link
+                href="https://wanchanlin.webflow.io/work/rakuten"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="companylogo"
+                  src="/images/rakuten.svg"
+                  alt="Rakuten"
+                  alt="XingMobility"
+                />
               </Link>
-              <Link href="https://wanchanlin.webflow.io/work/hongmall" target="_blank" rel="noopener noreferrer">
-                <Image className="companylogo" src="/images/spaceship.svg" alt="XingMobility" width={0} height={0} style={{width: 'auto', height: '50px'}} />
+              <Link
+                href="https://wanchanlin.webflow.io/work/hongmall"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="companylogo"
+                  src="/images/spaceship.svg"
+                  alt="XingMobility"
+                />
               </Link>
-              <Link href="https://wanchanlin.webflow.io/work/hongmall" target="_blank" rel="noopener noreferrer">
-                <Image className="companylogo" src="/images/HongMall.svg" alt="HongMall" width={0} height={0}  style={{width: 'auto', height: '50px'}} />
+              <Link
+                href="https://wanchanlin.webflow.io/work/hongmall"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="companylogo" src="/images/HongMall.svg" />
               </Link>
-              <Image className="companylogo" src="/images/heys.svg" alt="Heys" width={0} height={0} sizes='20vw' style={{width: 'auto', height: '50px'}}  />
-              <Link href="https://wanchanlin.webflow.io/work/xingmobility" target="_blank" rel="noopener noreferrer">
-                <Image className="companylogo" src="/images/xingmobility.svg" alt="XingMobility" width={0} height={0} style={{width: 'auto', height: '50px'}}  />
+              <img className="companylogo" src="/images/heys.svg" alt="Heys" />
+              <Link
+                href="https://wanchanlin.webflow.io/work/xingmobility"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="companylogo"
+                  src="/images/xingmobility.svg"
+                  alt="XingMobility"
+                />
               </Link>
             </div>
           </div>
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 export default function TermsPage() {
   return (
-    <main className="min-h-screen py-8">
-      <div className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold mb-8">Terms of Service</h1>
+    <main>
+      <div>
+        <h1>Terms of Service</h1>
         
-        <div className="prose max-w-none">
-          <section className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">1. Acceptance of Terms</h2>
+        <div>
+          <section>
+            <h2>1. Acceptance of Terms</h2>
             <p>
               By accessing and using this website, you accept and agree to be bound by the terms and provision of this agreement.
             </p>
           </section>
 
-          <section className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">2. Use License</h2>
+          <section>
+            <h2>2. Use License</h2>
             <p>
               Permission is granted to temporarily download one copy of the materials (information or software) on our website
               for personal, non-commercial transitory viewing only.
             </p>
           </section>
 
-          <section className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">3. Disclaimer</h2>
+          <section>
+            <h2>3. Disclaimer</h2>
             <p>
               The materials on our website are provided on an 'as is' basis. We make no warranties, expressed or implied,
               and hereby disclaim and negate all other warranties including, without limitation, implied warranties or
@@ -32,8 +32,8 @@ export default function TermsPage() {
             </p>
           </section>
 
-          <section className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">4. Limitations</h2>
+          <section>
+            <h2>4. Limitations</h2>
             <p>
               In no event shall we or our suppliers be liable for any damages (including, without limitation, damages for
               loss of data or profit, or due to business interruption) arising out of the use or inability to use the
@@ -41,16 +41,16 @@ export default function TermsPage() {
             </p>
           </section>
 
-          <section className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">5. Accuracy of Materials</h2>
+          <section>
+            <h2>5. Accuracy of Materials</h2>
             <p>
               The materials appearing on our website could include technical, typographical, or photographic errors.
               We do not warrant that any of the materials on our website are accurate, complete, or current.
             </p>
           </section>
 
-          <section className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">6. Links</h2>
+          <section>
+            <h2>6. Links</h2>
             <p>
               We have not reviewed all of the sites linked to our website and are not responsible for the contents of any
               such linked site. The inclusion of any link does not imply endorsement by us of the site.
@@ -58,7 +58,7 @@ export default function TermsPage() {
           </section>
 
           <section>
-            <h2 className="text-2xl font-bold mb-4">7. Modifications</h2>
+            <h2>7. Modifications</h2>
             <p>
               We may revise these terms of service for our website at any time without notice. By using this website,
               you are agreeing to be bound by the then current version of these terms of service.
